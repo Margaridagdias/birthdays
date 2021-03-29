1c6b972803914b90c44edd7bea1aa95b3feaf19d
Margarida <margarida97pt@gmail.com>
2021-03-29 17:05:33 +0100

    final commit

---
+++ src/data.js
@@ -0,0 +1,37 @@
+export default [
+  {
+    id: 1,
+    name: "Beyoncé Knowles",
+    age: 39,
+    image:
+      "https://s2.glbimg.com/7oMjs3qw9yXfha6RJkC7z326M7w=/620x620/smart/e.glbimg.com/og/ed/f/original/2018/08/30/beyonce011-vogueint-28aug18-rexfeaturees_2.jpg",
+  },
+  {
+    id: 2,
+    name: "Rihanna",
+    age: 33,
+    image:
+      "https://brazil-grlk5lagedl.stackpathdns.com/production/brazil/images/1597413012110543-rihanna-fenty-beauty-brasil.PNG?w=1900&fit=crop&crop=faces&auto=%5B%22format%22%2C%20%22compress%22%5D&cs=srgb",
+  },
+  {
+    id: 3,
+    name: "Jay-Z",
+    age: 51,
+    image:
+      "https://thumbor.forbes.com/thumbor/fit-in/416x416/filters%3Aformat%28jpg%29/https%3A%2F%2Fspecials-images.forbesimg.com%2Fimageserve%2F5ed554fab14861000600baf1%2F0x0.jpg%3Fbackground%3D000000%26cropX1%3D53%26cropX2%3D739%26cropY1%3D114%26cropY2%3D800",
+  },
+  {
+    id: 4,
+    name: "Megan Thee Stallion",
+    age: 26,
+    image:
+      "https://ca-times.brightspotcdn.com/dims4/default/c25796d/2147483647/strip/true/crop/6257x5340+0+0/resize/1486x1268!/quality/90/?url=https%3A%2F%2Fcalifornia-times-brightspot.s3.amazonaws.com%2Fca%2F6d%2F3e62fae04ff8bde6fe68cbb0cb18%2Fmegan-thee-stallion-pc-marcelo-cantu.jpg",
+  },
+  {
+    id: 5,
+    name: "Ariana Grande",
+    age: 27,
+    image:
+      "https://famouspeople.wiki/wp-content/uploads/2019/12/ariana-grande.jpg",
+  },
+];
